1dfb38e1838130f02e85150f5513e0894eca9ccd
kevin wu <kevinray.ee12@nycu.edu.tw>
2024-06-12 00:52:18 +0800

    set background from Kevin

--- bomb_main.py
+++ bomb_main.py
@@ -114,3 +114,66 @@ class GameStatus(Enum):
 def print_text(screen, font, x, y, text, fcolor=(255, 255, 255)):
     imgText = font.render(text, True, fcolor)
     screen.blit(imgText, (x, y))
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('扫雷')
+
+    font1 = pygame.font.Font('resources/a.TTF', SIZE * 2)
+    fwidth, fheight = font1.size('999')
+    red = (200, 40, 40)
+    img0 = pygame.image.load('resources/0.bmp').convert()
+    img0 = pygame.transform.smoothscale(img0, (SIZE, SIZE))
+    img1 = pygame.image.load('resources/1.bmp').convert()
+    img1 = pygame.transform.smoothscale(img1, (SIZE, SIZE))
+    img2 = pygame.image.load('resources/2.bmp').convert()
+    img2 = pygame.transform.smoothscale(img2, (SIZE, SIZE))
+    img3 = pygame.image.load('resources/3.bmp').convert()
+    img3 = pygame.transform.smoothscale(img3, (SIZE, SIZE))
+    img4 = pygame.image.load('resources/4.bmp').convert()
+    img4 = pygame.transform.smoothscale(img4, (SIZE, SIZE))
+    img5 = pygame.image.load('resources/5.bmp').convert()
+    img5 = pygame.transform.smoothscale(img5, (SIZE, SIZE))
+    img6 = pygame.image.load('resources/6.bmp').convert()
+    img6 = pygame.transform.smoothscale(img6, (SIZE, SIZE))
+    img7 = pygame.image.load('resources/7.bmp').convert()
+    img7 = pygame.transform.smoothscale(img7, (SIZE, SIZE))
+    img8 = pygame.image.load('resources/8.bmp').convert()
+    img8 = pygame.transform.smoothscale(img8, (SIZE, SIZE))
+    img_blank = pygame.image.load('resources/blank.bmp').convert()
+    img_blank = pygame.transform.smoothscale(img_blank, (SIZE, SIZE))
+    img_flag = pygame.image.load('resources/flag.bmp').convert()
+    img_flag = pygame.transform.smoothscale(img_flag, (SIZE, SIZE))
+    img_ask = pygame.image.load('resources/ask.bmp').convert()
+    img_ask = pygame.transform.smoothscale(img_ask, (SIZE, SIZE))
+    img_mine = pygame.image.load('resources/mine.bmp').convert()
+    img_mine = pygame.transform.smoothscale(img_mine, (SIZE, SIZE))
+    img_blood = pygame.image.load('resources/blood.bmp').convert()
+    img_blood = pygame.transform.smoothscale(img_blood, (SIZE, SIZE))
+    img_error = pygame.image.load('resources/error.bmp').convert()
+    img_error = pygame.transform.smoothscale(img_error, (SIZE, SIZE))
+    face_size = int(SIZE * 1.25)
+    img_face_fail = pygame.image.load('resources/face_fail.bmp').convert()
+    img_face_fail = pygame.transform.smoothscale(img_face_fail, (face_size, face_size))
+    img_face_normal = pygame.image.load('resources/face_normal.bmp').convert()
+    img_face_normal = pygame.transform.smoothscale(img_face_normal, (face_size, face_size))
+    img_face_success = pygame.image.load('resources/face_success.bmp').convert()
+    img_face_success = pygame.transform.smoothscale(img_face_success, (face_size, face_size))
+    face_pos_x = (SCREEN_WIDTH - face_size) // 2
+    face_pos_y = (SIZE * 2 - face_size) // 2
+    img_dict = {
+        0: img0,
+        1: img1,
+        2: img2,
+        3: img3,
+        4: img4,
+        5: img5,
+        6: img6,
+        7: img7,
+        8: img8
+    }
+    bgcolor = (225, 225, 225)
+    block = MineBlock()
+    game_status = GameStatus.readied
+    start_time = None
+    elapsed_time = 0    
