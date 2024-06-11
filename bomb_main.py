5aebc1935504667e33b85d3d638053f397fc7be0
kevin wu <kevinray.ee12@nycu.edu.tw>
2024-06-11 22:42:45 +0800

    define bomb map from Kevin

--- bomb_main.py
+++ bomb_main.py
@@ -7,5 +7,12 @@ class BlockStatus(Enum):
     bomb = 6    # hit the bomb
     hint = 7    # neighbor has bomb
     double = 8  # being clicked twice
-
+class Mine:
+    def __init__(self, x, y, value=0):
+        self._x = x
+        self._y = y
+        self._value = 0
+        self._around_mine_count = -1
+        self._status = BlockStatus.normal
+        self.set_value(value)
 
