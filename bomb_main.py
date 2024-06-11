1c72e8c9812f53a39107aea99c30bd7dc0758dd4
kevin wu <kevinray.ee12@nycu.edu.tw>
2024-06-11 22:45:43 +0800

    define bomb map's coordinate from Kevin

--- bomb_main.py
+++ bomb_main.py
@@ -15,4 +15,23 @@ class Mine:
         self._around_mine_count = -1
         self._status = BlockStatus.normal
         self.set_value(value)
-
+    def __repr__(self):
+        return str(self._value)
+    def get_x(self):
+        return self._x
+    def set_x(self, x):
+        self._x = x
+    x = property(fget=get_x, fset=set_x)
+    def get_y(self):
+        return self._y
+    def set_y(self, y):
+        self._y = y
+    y = property(fget=get_y, fset=set_y)
+    def get_value(self):
+        return self._value
+    def set_value(self, value):
+        if value:
+            self._value = 1
+        else:
+            self._value = 0
+    value = property(fget=get_value, fset=set_value, doc='0:非地雷 1:雷')
