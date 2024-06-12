917f9a90a39b6c2293d556a22e5c80e28a36c01e
minopeng <mino.ee12@nycu.edu.tw>
2024-06-13 01:01:57 +0800

    deleter game status

--- bomb_main.py
+++ bomb_main.py
@@ -129,10 +129,3 @@ class MineTile:
     def _get_around(self, x, y):
         return [(i, j) for i in range(max(0, x - 1), min(TILE_WIDTH - 1, x + 1) + 1)
                 for j in range(max(0, y - 1), min(TILE_HIGHT - 1, y + 1) + 1) if i != x or j != y]
-
-class GameStatus(Enum):
-    readied = 1,
-    started = 2,
-    over = 3,
-    win = 4
-
