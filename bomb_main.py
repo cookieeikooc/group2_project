e395c6644d222238d304b2bc87eaf1746b48ec0a
minopeng <mino.ee12@nycu.edu.tw>
2024-06-13 01:22:24 +0800

    delete tile status

--- bomb_main.py
+++ bomb_main.py
@@ -13,12 +13,3 @@ MINE_COUNT = 10  # Adjust the mine count as necessary
 SCREEN_WIDTH = TILE_WIDTH * SIZE
 SCREEN_HEIGHT = (TILE_HIGHT + 2) * SIZE
 
-class TileStatus(Enum):
-    idle = 1  # not clicked
-    clicked = 2  # clicked
-    mine = 3    # bomb
-    flag = 4    # bomb mark
-    ask = 5     # question mark
-    bomb = 6    # hit the bomb
-    hint = 7    # neighbor has bomb
-    double = 8  # being clicked twice   
