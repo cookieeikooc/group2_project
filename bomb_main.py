765ddc0afa6fcf0fafc4b390d9b454765f0fee41
minopeng <mino.ee12@nycu.edu.tw>
2024-06-13 01:23:15 +0800

    delete constants

--- bomb_main.py
+++ bomb_main.py
@@ -5,11 +5,3 @@ from enum import Enum
 import pygame
 from pygame.locals import *
 
-# Constants
-TILE_WIDTH = 10  # Adjust the width as necessary
-TILE_HIGHT = 10  # Adjust the height as necessary
-SIZE = 20  # Adjust the tile size as necessary
-MINE_COUNT = 10  # Adjust the mine count as necessary
-SCREEN_WIDTH = TILE_WIDTH * SIZE
-SCREEN_HEIGHT = (TILE_HIGHT + 2) * SIZE
-
