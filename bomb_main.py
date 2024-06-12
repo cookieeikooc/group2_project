07d1253c3d81714cc4e38a15174fdd6de21af60d
minopeng <mino.ee12@nycu.edu.tw>
2024-06-13 01:02:41 +0800

    deleter game around

--- bomb_main.py
+++ bomb_main.py
@@ -125,7 +125,3 @@ class MineTile:
         for i, j in self._get_around(x, y):
             if self._tile[j][i].status == TileStatus.hint:
                 self._tile[j][i].status = TileStatus.idle
-    
-    def _get_around(self, x, y):
-        return [(i, j) for i in range(max(0, x - 1), min(TILE_WIDTH - 1, x + 1) + 1)
-                for j in range(max(0, y - 1), min(TILE_HIGHT - 1, y + 1) + 1) if i != x or j != y]
