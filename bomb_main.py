48541eab8cf4a93873a1db01390b6534d19b65e8
minopeng <mino.ee12@nycu.edu.tw>
2024-06-13 00:52:39 +0800

    delete face running

--- bomb_main.py
+++ bomb_main.py
@@ -160,4 +160,3 @@ def main():
     img_error = pygame.transform.smoothscale(pygame.image.load('pictures/error.bmp').convert(), (SIZE, SIZE))
     face_size = int(SIZE * 1.25)
     img_face_lose = pygame.transform.smoothscale(pygame.image.load('pictures/face_lose.bmp').convert(), (face_size, face_size))
-    img_face_running = pygame.transform.smoothscale(pygame.image.load('pictures/face_running.bmp').convert(), (face_size, face_size))
