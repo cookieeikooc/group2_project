3cb12f24a50100be9c57781aca64e765c7617763
cookieeikooc <bingyuuygnib@gmail.com>
2024-06-16 03:38:09 +0800

    fix syntax

--- jocker_main.py
+++ jocker_main.py
@@ -4,7 +4,6 @@ import random
 import threading
 import pygame
 
-
 ### PyGame Setup ###
 #Pygame
 pygame.init()
@@ -82,7 +81,8 @@ class Dinosaur:
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
-        
+    
+    #State
     def update(self, userInput):
         if self.dino_duck:
             self.duck()
@@ -170,25 +170,27 @@ class Cloud:
 ### Bird ###
 class Bird(Obstacle):
     BIRD_HEIGHTS = [250, 290, 320]
+
     def __init__(self, image):
         self.type = 0
         super().__init__(image, self.type)
         self.rect.y = random.choice(self.BIRD_HEIGHTS)
-        self.index = 0 
+        self.index = 0
+
     def draw(self, SCREEN):
         if self.index >= 9:
             self.index = 0
         SCREEN.blit(self.image[self.index // 5], self.rect)
         self.index += 1
 
-### LargeCactus ###
+### LargeObs ###
 class LargeCactus(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
         self.rect.y = 300
         
-### SmallCactus ###
+### SmallObs ###
 class SmallCactus(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
